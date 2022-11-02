c72ecf95012a2e1da448c59f68bed358723167be
hZorah <heitorzorah@gmail.com>
2022-11-02 10:36:19 -0300

    Upgrade to URP

---
+++ Assets/Scripts/Tests/Edit/CiFirstTest.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+public class CiFirstTest
+{
+    
+    [Test]
+    public void CiFirstTestSimplePasses()
+    {
+        Assert.Pass();
+    }
+
+    
+}
